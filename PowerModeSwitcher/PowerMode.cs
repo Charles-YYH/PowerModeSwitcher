7d0c7fc3d79e42c957c78895ecf3c32ae79e8d59
qyxx01 <qyxx01@icloud.com>
2023-11-26 11:22:54 +0100

    feat: add example code to change power mode based on cursor activity

--- PowerModeSwitcher/PowerMode.cs
+++ PowerModeSwitcher/PowerMode.cs
@@ -5,8 +5,13 @@ namespace PowerModeSwitcher;
 
 public static class PowerMode
 {
+    public static readonly Guid BestPowerEfficiency = new("961cc777-2547-4f9d-8174-7d86181b8a7a");
+    public static readonly Guid Balanced = new("00000000-0000-0000-0000-000000000000");
+    public static readonly Guid BestPerformance = new("ded574b5-45a0-4f42-8737-46345c09c238");
+    
     public static bool TrySetPowerMode(Guid id)
     {
+        if (GetPowerMode() == id) return false;
         return PowerSetActiveOverlayScheme(id) == 0;
     }
 
--- PowerModeSwitcher/Program.cs
+++ PowerModeSwitcher/Program.cs
@@ -1,6 +1,30 @@
-﻿namespace PowerModeSwitcher;
+﻿using System.Drawing;
+
+namespace PowerModeSwitcher;
 
 internal abstract class Program
 {
-    private static void Main() { }
+    private const int HistoryPointCount = 120;
+    private const int Interval = 1000;
+    private static readonly Point[] _points = new Point[HistoryPointCount];
+
+    // Set power mode to best power efficiency if idle for 120s
+    private static void Main()
+    {
+        var tick = 0;
+        while (true)
+        {
+            _points[tick++ % HistoryPointCount] = Mouse.GetCursorPosition();
+            Thread.Sleep(Interval);
+            var powerMode = CheckActivity() ? PowerMode.Balanced : PowerMode.BestPowerEfficiency;
+            PowerMode.TrySetPowerMode(powerMode);
+        }
+    }
+
+    private static bool CheckActivity()
+    {
+        if (_points.Any(point => point.Equals(default))) return true;
+        var first = _points.First();
+        return _points.Any(point => point != first);
+    }
 }
